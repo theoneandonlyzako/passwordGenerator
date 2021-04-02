a49a152a4aec2a9e67f06531ac46c066138af51d
Zak Stamps <theoneandonlyzako@gmail.com>
2021-04-02 16:49:26 -0700

    started adding js

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 // Assignment code here
+var length = Number(prompt("How many characters would you like your password to be?"));
+while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("Length must be 8-128 characters. How many characters would you like your password to be?"));
+
+var uppers = confirm("Would you like to use uppercase letters?");
+var lowers = confirm("Would you like to use lowercase letters?");
+var numbers = confirm("Would you like to use numbers?");
+var symbols = confirm("Would you like to use special characters?");
+
+while (!uppers && !lowers && !numbers && !symbols) {
+  alert("You must select at least one character type!");
+  uppers = confirm("Would you like to use uppercase letters?");
+  lowers = confirm("Would you like to use lowercase letters?");
+  numbers = confirm("Would you like to use numbers?");
+  symbols = confirm("Would you like to use special characters?");
+}
 
 
 // Get references to the #generate element
